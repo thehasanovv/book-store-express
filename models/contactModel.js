a85bd7e242b823fbf0488d6803daea645c523180
thehasanovv <hasanov.e95@gmail.com>
2022-04-18 17:25:46 +0400

    contact_us post method

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,28 +1,28 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Name is required"],
+    required: [true, 'Name is required'],
     trim: true,
-    maxlength: [25, "Name must have less or equal than 25 characters"],
-    minlength: [6, "Name must have more or equal than 6 characters"],
+    maxlength: [25, 'Name must have less or equal than 25 characters'],
+    minlength: [6, 'Name must have more or equal than 6 characters'],
   },
   surname: {
     type: String,
-    required: [true, "Surname is required"],
+    required: [true, 'Surname is required'],
     trim: true,
-    maxlength: [25, "Name must have less or equal than 25 characters"],
-    minlength: [6, "Name must have more or equal than 6 characters"],
+    maxlength: [25, 'Name must have less or equal than 25 characters'],
+    minlength: [6, 'Name must have more or equal than 6 characters'],
   },
   email: {
     type: String,
-    required: [true, "Email is required"],
+    required: [true, 'Email is required'],
   },
-  phone_number: {
+  phone: {
     type: String,
-    required: [true, "Number is required"],
+    required: [true, 'Number is required'],
   },
 });
 
-module.exports = mongoose.model("Contact", contactSchema);
+module.exports = mongoose.model('Contact', contactSchema);
--- router/contactRoutes.js
+++ router/contactRoutes.js
@@ -7,13 +7,16 @@ router.get('/', (req, res) => {
   res.render('site/contactus');
 });
 
-// router.post('/', async (req, res) => {
-//   const newContact = await ContactModel.create(req.body);
-//   res.redirect('/contact');
-// });
-
-router.post('/', (req, res) => {
-  ContactModel.create(req.body, (err, contact) => {});
+router.post('/', async (req, res) => {
+  const newContact = await ContactModel.create(req.body);
+  console.log(newContact);
+  res.redirect('/contact');
 });
+
+// router.post('/', (req, res) => {
+//   ContactModel.create(req.body, (err, contact) => {
+//     res.redirect('/contact');
+//   });
+// });
 
 module.exports = router;
