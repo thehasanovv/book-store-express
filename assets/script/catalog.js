fc799447716c07fa3f480667df4a200be419c22d
thehasanovv <hasanov.e95@gmail.com>
2022-04-22 19:19:13 +0400

    each catalog

--- assets/script/catalog.js
+++ assets/script/catalog.js
@@ -1,18 +1,12 @@
-for (let result in books) {
-  var books = data[result];
-  if (books.bookType === `${bookType}`) {
-    let newBook = $(`<div class = "rounded text-center">
-        <div class="card shadow rounded" style="width: 15rem">
-            <img class="card-img-top img-fluid " src="${books.imageUrl}" alt="Card image cap" style="height: 250px; object-fit: cover;">
-            <div class="card-body ">
-                <h6 class="card-title text-center book-name" style="height:40px">${books.bookName}</h6>
-                <p class="card-text text-center" style="height: 50px; font-size:14px">${books.authorName}</p>
-                <button class="btn btn-primary read-more" data-name="${books.bookName}" style="font-size:16px;">Read more</button>
-            </div>
-        </div>
-    </div>
-    `);
+// $(document).ready(function () {});
 
-    $(emptyDiv).append(newBook);
-  }
-}
+// $('.catalog-card').on('click', function (e) {
+//   const clicked = e.target.closest('.read-more-btn');
+//   if (!clicked) return;
+
+//   const id = $(this).data('id');
+//   // window.location = `/catalog/${id}`;
+//   // clicked.href = `/catalog/${id}`;
+//   console.log(id);
+//   console.log(clicked);
+// });
