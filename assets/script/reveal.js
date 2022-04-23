1d749c59826be4402a7d064de0372d862467bfa1
thehasanovv <hasanov.e95@gmail.com>
2022-04-23 15:40:36 +0400

    observer & animation & css responsive

--- assets/script/reveal.js
+++ assets/script/reveal.js
@@ -1,6 +1,51 @@
-ScrollReveal().reveal('.reveal', {
+////////////////////////////
+// Scroll Reveal
+const reveal = ScrollReveal();
+
+reveal.reveal('.reveal', {
   reset: true,
   duration: 1500,
   delay: 400,
   distance: '60px',
 });
+
+console.log(reveal);
+////////////////////////////
+// Observer & Animate numbers
+let target = document.querySelector('.about-store-section');
+
+const obsCallBack = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  numbersAnimation();
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+};
+
+const observer = new IntersectionObserver(obsCallBack, obsOptions);
+observer.observe(target);
+
+function numbersAnimation() {
+  const counters = document.querySelectorAll('.value');
+  const speed = 500;
+
+  counters.forEach((counter) => {
+    const animate = () => {
+      const value = +counter.getAttribute('akhi');
+      const data = +counter.innerText;
+      const time = value / speed;
+      if (data < value) {
+        counter.innerText = Math.ceil(data + time);
+        setTimeout(animate, 1);
+      } else {
+        counter.innerText = value;
+      }
+    };
+
+    animate();
+  });
+}
--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Category is required'],
-    },
+// const categorySchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: [true, 'Category is required'],
+//     },
 
-});
+// });
 
-module.exports = mongoose.model('Category', categorySchema);
+// module.exports = mongoose.model('Category', categorySchema);
