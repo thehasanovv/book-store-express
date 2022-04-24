b98cbb104d7d99c41f567cf3a4a08612ffc39c55
thehasanovv <hasanov.e95@gmail.com>
2022-04-25 01:55:40 +0400

    search

--- router/searchRoutes.js
+++ router/searchRoutes.js
@@ -1,16 +1,21 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const BooksModel = require('../models/bookModel')
+const BooksModel = require('../models/bookModel');
 
-router.get("/", (req, res) => {
-  res.render("site/search");
+router.get('/', (req, res) => {
+  res.render('site/search');
 });
 
 router.post('/', async (req, res) => {
   const bookName = req.body.search;
-  const books = await BooksModel.aggregate([{$match: {bookName}}]);
+  let regex = new RegExp(`^${bookName}`, 'ig');
+  const books = await BooksModel.aggregate([{ $match: { bookName: regex } }]);
+  // const books = await BooksModel.find({
+  //   bookName: { $regex: /Harry$/, $match: 'Harry' },
+  // });
+
   console.log(books);
-  res.render('site/search', {books});
-})
+  res.render('site/search', { books });
+});
 
 module.exports = router;
