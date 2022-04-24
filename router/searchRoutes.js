4cb227126a53f050aef7ecfdef885b836bc99f83
huseynahmadov <t.huseyn96@mail.ru>
2022-04-25 00:37:22 +0400

    search section processing

--- router/searchRoutes.js
+++ router/searchRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const BooksModel = require('../models/bookModel')
 
 router.get("/", (req, res) => {
   res.render("site/search");
 });
+
+router.post('/', async (req, res) => {
+  const bookName = req.body.search;
+  const books = await BooksModel.aggregate([{$match: {bookName}}]);
+  console.log(books);
+  res.render('site/search', {books});
+})
 
 module.exports = router;
