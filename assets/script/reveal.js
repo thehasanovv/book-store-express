f4bd6d9ac313fa63bbafd3e87d70849cb639ca6e
thehasanovv <hasanov.e95@gmail.com>
2022-04-24 15:09:04 +0400

    update

--- assets/script/reveal.js
+++ assets/script/reveal.js
@@ -31,13 +31,14 @@ observer.observe(target);
 
 function numbersAnimation() {
   const counters = document.querySelectorAll('.value');
-  const speed = 500;
+  let speed = 500;
 
   counters.forEach((counter) => {
     const animate = () => {
       const value = +counter.getAttribute('akhi');
       const data = +counter.innerText;
       const time = value / speed;
+
       if (data < value) {
         counter.innerText = Math.ceil(data + time);
         setTimeout(animate, 1);
